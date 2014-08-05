cf4763f2bb8d8cbe85899039857271dd00462b10
Daniel Havlik <dh@gocept.com>
2014-08-05 10:54:26 +0200

    update library to 9.7.0

--- js/jquery_fileupload/__init__.py
+++ js/jquery_fileupload/__init__.py
@@ -6,37 +6,12 @@ import js.jqueryui
 
 library = Library('jquery_fileupload', 'resources')
 
-jquery_getimagedata = Resource(
-    library, 'vendor/jquery.getimagedata.min.js',
-)
-
-tmpl_js = Resource(
-    library, 'js/tmpl.min.js',
-    depends=[
-        js.jqueryui.ui_widget,
-        jquery_getimagedata,
-    ],
-)
-
-load_image_js = Resource(
-    library, 'js/load-image.min.js',
-    depends=[
-        tmpl_js,
-    ],
-)
-
-canvas_to_blob_js = Resource(
-    library, 'js/canvas-to-blob.min.js',
-    depends=[
-        load_image_js,
-    ],
-)
+ui_widget_js = Resource(
+    library, 'js/vendor/jquery.ui.widget.js')
 
 iframe_transport_js = Resource(
     library, 'js/jquery.iframe-transport.js',
-    depends=[
-        canvas_to_blob_js,
-    ],
+    depends=[ui_widget_js],
 )
 
 fileupload_js = Resource(
@@ -73,6 +48,10 @@ fileupload_ui_css = Resource(
     library, 'css/jquery.fileupload-ui.css',
 )
 
+fileupload_css = Resource(
+    library, 'css/jquery.fileupload.css',
+)
+
 fileupload_ui_js = Resource(
     library, 'js/jquery.fileupload-ui.js',
     depends=[
@@ -81,6 +60,11 @@ fileupload_ui_js = Resource(
     ],
 )
 
-jquery_fileupload = Group([
+jquery_ui_fileupload = Group([
     fileupload_ui_js,
+])
+
+jquery_fileupload = Group([
+    fileupload_validate_js,
+    fileupload_css,
 ])
